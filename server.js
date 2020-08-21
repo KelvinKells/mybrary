418851c83b043375904b2c99531237d7a54163f0
Kelvin Marope <loicsjan@gmail.com>
2020-08-21 10:48:59 +0200

    Books Create/Index/New

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -32,6 +33,7 @@ mongoose.connect("mongodb://localhost:27017/mybrary",
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 // app.listen(process.env.PORT || 2000)
 app.listen(process.env.PORT || 2000, process.env.IP, function(){
